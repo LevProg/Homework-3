d2470bcc38874ee464da998b989789f095ab72c5
Лев Черняховский <levcernahovskij4419@gmail.com>
2023-11-09 13:15:44 +0300

    Update 3sem_2.cpp

--- 2/3sem_2.cpp
+++ 2/3sem_2.cpp
@@ -1,69 +1,69 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <list>
-#include <vector>
 
 using namespace std;
 
 struct MatrixElement {
-    int row;
     int col;
     int value;
 };
 
-void readMatrix(const string& filename, list<MatrixElement>& matrix, int n) {
+void readMatrix(const string& filename, list<MatrixElement> matrix[], int n) {
     ifstream file(filename);
     int value;
-
     for (int row = 0; row < n; row++) {
         for (int col = 0; col < n; col++) {
             file >> value;
-            if (value>0) matrix.push_back({ row, col, value });
+            if (value > 0) matrix[row].push_back({ col, value });
         }
     }
     file.close();
 }
 
-list<MatrixElement> combineSparseMatrices(
-    const list<MatrixElement>& matrix1,
-    const list<MatrixElement>& matrix2,
-    const list<MatrixElement>& matrix3,
-    const list<MatrixElement>& matrix4,
+void combineSparseMatrices(
+    list<MatrixElement> *result,
+    const list<MatrixElement> *matrix1,
+    const list<MatrixElement> *matrix2,
+    const list<MatrixElement> *matrix3,
+    const list<MatrixElement> *matrix4,
     int n) {
-    list<MatrixElement> result;
-
-    for (const auto& elem : matrix1) {
-        result.push_back({ elem.row, elem.col, elem.value });
+    for (int row = 0; row < n; row++) {
+        for (const auto& elem : matrix1[row]) {
+            result[row].push_back({ elem.col, elem.value });
+        }
+        for (const auto& elem : matrix2[row]) {
+            result[row].push_back({ elem.col + n, elem.value });
+        }
     }
-    for (const auto& elem : matrix2) {
-        result.push_back({ elem.row, elem.col + n, elem.value });
+    for (int row = n; row < 2*n; row++) {
+        for (const auto& elem : matrix3[row-n]) {
+            result[row].push_back({ elem.col, elem.value });
+        }
+        for (const auto& elem : matrix4[row - n]) {
+            result[row].push_back({ elem.col + n, elem.value });
+        }
     }
-    for (const auto& elem : matrix3) {
-        result.push_back({ elem.row + n, elem.col, elem.value });
-    }
-    for (const auto& elem : matrix4) {
-        result.push_back({ elem.row + n, elem.col + n, elem.value });
-    }
-    return result;
+    
 }
 
 int main() {
-    int n = 2;
+    const int n = 2;
 
-    list<MatrixElement> matrix1, matrix2, matrix3, matrix4;
-
+    list<MatrixElement> matrix1[n], matrix2[n], matrix3[n], matrix4[n];
+    list<MatrixElement> result[n * 2];
     readMatrix("matrix1.txt", matrix1, n);
     readMatrix("matrix2.txt", matrix2, n);
     readMatrix("matrix3.txt", matrix3, n);
     readMatrix("matrix4.txt", matrix4, n);
 
-    list<MatrixElement> result = combineSparseMatrices(matrix1, matrix2, matrix3, matrix4, n);
+    combineSparseMatrices(result, matrix1, matrix2, matrix3, matrix4, n);
 
-    for (int i = 0; i < 2 * n; i++) {
+    for (int row = 0; row < 2 * n; row++) {
         for (int j = 0; j < 2 * n; j++) {
             bool found = false;
-            for (const auto& elem : result) {
-                if (elem.row == i && elem.col == j) {
+            for (const auto& elem : result[row]) {
+                if (elem.col == j) {
                     cout << elem.value << " ";
                     found = true;
                     break;
