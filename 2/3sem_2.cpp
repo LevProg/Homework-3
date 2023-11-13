97e868c39daf9d94c1c690ffdf917887df57fc85
Лев Черняховский <levcernahovskij4419@gmail.com>
2023-11-14 01:58:10 +0300

    Update 3sem_2.cpp

--- 2/3sem_2.cpp
+++ 2/3sem_2.cpp
@@ -1,57 +1,97 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <list>
 
 using namespace std;
 
 struct MatrixElement {
     int col;
     int value;
+    MatrixElement* next;
 };
 
-void readMatrix(const string& filename, list<MatrixElement> matrix[], int n) {
+void insertSorted(MatrixElement*& head, int col, int value) {
+    MatrixElement* newNode = new MatrixElement{ col, value, nullptr };
+    if (!head || head->col > col) {
+        newNode->next = head;
+        head = newNode;
+    }
+    else {
+        MatrixElement* temp = head;
+        while (temp->next && temp->next->col < col) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void readMatrix(const string& filename, MatrixElement* matrix[], int n) {
     ifstream file(filename);
     int value;
     for (int row = 0; row < n; row++) {
         for (int col = 0; col < n; col++) {
             file >> value;
-            if (value > 0) matrix[row].push_back({ col, value });
+            if (value != 0) insertSorted(matrix[row], col, value);
         }
     }
     file.close();
 }
 
 void combineSparseMatrices(
-    list<MatrixElement> *result,
-    const list<MatrixElement> *matrix1,
-    const list<MatrixElement> *matrix2,
-    const list<MatrixElement> *matrix3,
-    const list<MatrixElement> *matrix4,
+    MatrixElement* result[],
+    const MatrixElement* const matrix1[],
+    const MatrixElement* const matrix2[],
+    const MatrixElement* const matrix3[],
+    const MatrixElement* const matrix4[],
     int n) {
     for (int row = 0; row < n; row++) {
-        for (const auto& elem : matrix1[row]) {
-            result[row].push_back({ elem.col, elem.value });
+        const MatrixElement* current = matrix1[row];
+        while (current) {
+            insertSorted(result[row], current->col, current->value);
+            current = current->next;
         }
-        for (const auto& elem : matrix2[row]) {
-            result[row].push_back({ elem.col + n, elem.value });
-        }
-    }
-    for (int row = n; row < 2*n; row++) {
-        for (const auto& elem : matrix3[row-n]) {
-            result[row].push_back({ elem.col, elem.value });
-        }
-        for (const auto& elem : matrix4[row - n]) {
-            result[row].push_back({ elem.col + n, elem.value });
+        current = matrix2[row];
+        while (current) {
+            insertSorted(result[row], current->col + n, current->value);
+            current = current->next;
         }
     }
-    
+    for (int row = n; row < 2 * n; row++) {
+        const MatrixElement* current = matrix3[row - n];
+        while (current) {
+            insertSorted(result[row], current->col, current->value);
+            current = current->next;
+        }
+
+        current = matrix4[row - n];
+        while (current) {
+            insertSorted(result[row], current->col + n, current->value);
+            current = current->next;
+        }
+    }
+}
+
+void printMatrix(const MatrixElement* const matrix[], int rows, int cols) {
+    for (int row = 0; row < rows; row++) {
+        const MatrixElement* current = matrix[row];
+        for (int j = 0; j < cols; j++) {
+            if (current && current->col == j) {
+                cout << current->value << " ";
+                current = current->next;
+            }
+            else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
 }
 
 int main() {
     const int n = 2;
 
-    list<MatrixElement> matrix1[n], matrix2[n], matrix3[n], matrix4[n];
-    list<MatrixElement> result[n * 2];
+    MatrixElement* matrix1[n] = { nullptr }, * matrix2[n] = { nullptr }, * matrix3[n] = { nullptr }, * matrix4[n] = { nullptr };
+    MatrixElement* result[2 * n] = { nullptr };
     readMatrix("matrix1.txt", matrix1, n);
     readMatrix("matrix2.txt", matrix2, n);
     readMatrix("matrix3.txt", matrix3, n);
@@ -59,21 +99,7 @@ int main() {
 
     combineSparseMatrices(result, matrix1, matrix2, matrix3, matrix4, n);
 
-    for (int row = 0; row < 2 * n; row++) {
-        for (int j = 0; j < 2 * n; j++) {
-            bool found = false;
-            for (const auto& elem : result[row]) {
-                if (elem.col == j) {
-                    cout << elem.value << " ";
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                cout << "0 ";
-            }
-        }
-        cout << endl;
-    }
+    printMatrix(result, 2 * n, 2 * n);
+
     return 0;
 }
