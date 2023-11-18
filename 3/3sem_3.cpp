2128c4165ab1ac9663e96836342afa63a6ddfd6f
Лев Черняховский <levcernahovskij4419@gmail.com>
2023-11-19 01:51:51 +0300

    Update 3sem_3.cpp

--- 3/3sem_3.cpp
+++ 3/3sem_3.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-vector<vector<int>> board(8, vector<int>(8, 0));
-vector<int> queens(8, 0);
-int n = 0;
+vector<vector<int>> board;
+vector<int> queens;
+int n;
 
 void printSol() {
     ofstream file("output.txt");
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             if (board[i][j] < 0) {
                 file << i << ' ' << j << endl;
             }
@@ -20,27 +20,27 @@ void printSol() {
 }
 void markFields(int row, int col, int val) {
     int k = 0;
-    if (val == 0) k = row;
+    if (val == 0) k = col+1;
     else k = 0;
-    for (int j = 0; j < 8; j++) {
-        if (board[col][j] == 0) board[col][j] = val;
-        if (board[j][row] == 0) board[j][row] = val;
-        if (col + j < 8 && row + j < 8 && board[col + j][row + j] == k ) board[col + j][row + j] = val;
+    for (int j = 0; j < n; j++) {
+        if (board[col][j] == k) board[col][j] = val;
+        if (board[j][row] == k) board[j][row] = val;
+        if (col + j < n && row + j < n && board[col + j][row + j] == k) board[col + j][row + j] = val;
         if (col - j >= 0 && row - j >= 0 && board[col - j][row - j] == k) board[col - j][row - j] = val;
-        if (col + j < 8 && row - j >= 0 && board[col + j][row - j] == k) board[col + j][row - j] = val;
-        if (col - j >= 0 && row + j < 8 && board[col - j][row + j] == k) board[col - j][row + j] = val;
+        if (col + j < n && row - j >= 0 && board[col + j][row - j] == k) board[col + j][row - j] = val;
+        if (col - j >= 0 && row + j < n && board[col - j][row + j] == k) board[col - j][row + j] = val;
     }
     board[col][row] = -1 * val;
 }
 int chooseM() {
     int max = 0;
-    int col=0;
-    for (int j = 0; j < 8; j++) {
+    int col = 0;
+    for (int j = 0; j < n; j++) {
         int cur = 0;
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < n; i++) {
             if (board[i][j] != 0) cur++;
         }
-        if (cur > max && cur!=8) {
+        if (cur > max && cur != n) {
             max = cur;
             col = j;
         }
@@ -53,10 +53,10 @@ void findFields(int m) {
         exit(0);
     }
     else {
-        for (int col = 0; col < 8; col++) {
+        for (int col = 0; col < n; col++) {
             int row = chooseM();
             if (board[col][row] == 0) {
-                queens[m] = m+1;
+                queens[m] = col + 1;
                 markFields(row, col, queens[m]);
                 findFields(m + 1);
                 markFields(row, col, 0);
@@ -67,6 +67,8 @@ void findFields(int m) {
 int main() {
     ifstream file("input.txt");
     file >> n;
+    board=vector<vector<int>>(n, vector<int>(n, 0));
+    queens=vector<int>(n, 0);
     file.close();
     findFields(0);
     return 0;
